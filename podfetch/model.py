d4f805415134cc4b4e2184207a20d677d2624e2d
akeil <akeil@akeil.net>
2013-09-12 15:32:48 +0200

    Remove unused code.

--- podfetch/model.py
+++ podfetch/model.py
@@ -4,6 +4,7 @@ Podfetch Models
 
 Format for subscription files::
 
+    [subscription]
     url = http://example.com/feed
 
 '''
@@ -13,8 +14,6 @@ try:
     import configparser  # python 3
 except ImportError:
     import ConfigParser as configparser  # python 2
-
-import feedparser
 
 from podfetch.exceptions import NoSubscriptionError
 
@@ -27,25 +26,6 @@ class Subscription(object):
     def __init__(self, name, feed_url):
         self.name = name
         self.feed_url = feed_url
-
-    @property
-    def content_dir(self):
-        return os.path.join(self.content_root, self.name)
-
-    def update(self):
-        feed = feedparser.parse(self.feed_url)
-        self.update_episodes(feed)
-
-    def update_episodes(self, feed):
-        for entry in feed.entries:
-            self.process_one_entry(entry)
-
-    def process_one_entry(self, entry):
-        for index, enclosure in enumerate(entry.enclosures):
-            if self.is_downloaded(enclosure):
-                continue
-
-            dst_path = os.path.join(self.content_root, filename)
 
     def save(self, dirname):
         '''Save this subscription to an ini-file in the given
