ca0cd895b7091676f9a41f3971cd97df05b9c408
akeil <akeil@akeil.net>
2015-08-01 10:38:21 +0200

    version 0.4.7

--- podfetch/__init__.py
+++ podfetch/__init__.py
@@ -2,6 +2,6 @@
 # -*- coding: utf-8 -*-
 
 # remember setup.py
-__version__ = '0.4.7.dev2'
+__version__ = '0.4.7'
 __author__ = 'Alexander Keil'
 __email__ = 'alex@akeil.net'
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools.command.test import test as TestCommand
 
 
 # remember __init__.py
-VERSION = '0.4.7.dev2'
+VERSION = '0.4.7'
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
