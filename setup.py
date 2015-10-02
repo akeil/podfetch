9eaf25c0bfdd6a54ae11caae26fd9018df69a9b2
akeil <akeil@akeil.net>
2015-10-02 22:01:48 +0200

    support python 3.5

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     entry_points={
