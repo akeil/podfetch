488d00994a30f1123f250deed76ad1c60ec092a4
akeil <alex@akeil.net>
2019-08-23 23:12:04 +0200

    cleanup and add missing DBus methods

--- podfetch/__version__.py
+++ podfetch/__version__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-__version__ = '0.6.2.dev7'
+__version__ = '0.6.2.dev8'
--- podfetch/daemon/dbusapi.py
+++ podfetch/daemon/dbusapi.py
@@ -10,7 +10,9 @@ import dbus
 from dbus import service
 from dbus.mainloop.glib import DBusGMainLoop
 
+from podfetch.exceptions import NoSubscriptionError
 from podfetch.predicate import Filter
+from podfetch.predicate import NameFilter
 
 
 LOG = logging.getLogger(__name__)
@@ -48,6 +50,13 @@ class DBus:
         return '<DBus>'
 
 
+class NotFoundException(dbus.DBusException):
+    _dbus_error_name = 'de.akeil.Podfetch.NotFoundException'
+
+
+class InvalidArgumentException(dbus.DBusException):
+    _dbus_error_name = 'de.akeil.Podfetch.InvalidArgumentException'
+
 
 class _DBusPodfetch(dbus.service.Object):
 
@@ -55,21 +64,75 @@ class _DBusPodfetch(dbus.service.Object):
         super().__init__(bus, path)
         self._podfetch = podfetch
 
-    @dbus.service.method(_IFACE, in_signature='', out_signature='')
+    @dbus.service.method(_IFACE)
     def Update(self):
+        '''Trigger an update for all subscriptions.'''
         self._podfetch.update()
 
+    @dbus.service.method(_IFACE, in_signature='s')
+    def UpdateOne(self, name):
+        '''Trigger an update for a single subscription.'''
+        predicate = NameFilter(name)
+        self._podfetch.update(predicate=predicate)
+
     @dbus.service.method(_IFACE, in_signature='s', out_signature='a{ss}')
-    def Show(self, name):
-        sub = self._podfetch.subscription_for_name(name)
+    def ShowSubscription(self, name):
+        '''Show details for a subscription.'''
+        try:
+            sub = self._podfetch.subscription_for_name(name)
+        except NoSubscriptionError:
+            raise NotFoundException
+
         return {
             'name': sub.name,
             'title': sub.title,
         }
 
+    @dbus.service.method(_IFACE, in_signature='ss', out_signature='a{ss}')
+    def AddSubscription(self, url, name):
+        '''Subscribe to a new podcast.'''
+        try:
+            sub = self._podfetch.add_subscription(url, name=name)
+        except NoSubscriptionError:
+            raise NotFoundException
+
+        return {
+            'name': sub.name,
+            'title': sub.title,
+        }
+
+    @dbus.service.method(_IFACE, in_signature='sb')
+    def RemoveSubscription(self, name, delete_content):
+        '''Unsubscribe from a podcast
+        and optionally delete downloaded episodes.'''
+        try:
+            self._podfetch.remove_subscription(name,
+                delete_content=delete_content)
+        except NoSubscriptionError:
+            raise NotFoundException
+
+    @dbus.service.method(_IFACE, in_signature='s')
+    def EnableSubscription(self, name):
+        '''Set a subscription *enabled*.'''
+        try:
+            self._podfetch.edit(name, enabled=True)
+        except NoSubscriptionError:
+            raise NotFoundException
+
+    @dbus.service.method(_IFACE, in_signature='s')
+    def DisableSubscription(self, name):
+        '''Disable a subscription.'''
+        try:
+            self._podfetch.edit(name, enabled=False)
+        except NoSubscriptionError:
+            raise NotFoundException
+
     @dbus.service.method(_IFACE, in_signature='i', out_signature='a(sss(iiiiii)a(sss))')
     def Episodes(self, limit):
-        # TODO: check limit > 0
+        '''Show ``limit`` recent episodes.'''
+        if limit <= 0:
+            raise InvalidArgumentException('Limit must be a positive value.')
+
         accept = Filter()
 
         # fetch ALL episodes
