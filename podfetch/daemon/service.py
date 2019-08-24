9f42ab6eabe9c953e1182a11bb420cc2b14a3786
akeil <alex@akeil.net>
2019-08-24 10:48:29 +0200

    docs and cleanup

--- podfetch/daemon/service.py
+++ podfetch/daemon/service.py
@@ -13,17 +13,22 @@ LOG = logging.getLogger(__name__)
 
 
 def run(app, options):
-    daemon = Daemon(app, options)
+    '''Run a podfetch instance in daemon mode.
+
+    Will start all registered services and wait for SIGINT.
+    On SIGINT, services will be shutdown and the daemon stops.
+    '''
+    services = _ServiceManager(app, options)
 
     def on_sigint(sig, frame):
-        daemon.stop()
+        services.stop()
 
     signal.signal(signal.SIGINT, on_sigint)
-    daemon.start()
+    services.start()
     signal.pause()  # wait ...
 
 
-class Daemon:
+class _ServiceManager:
 
     def __init__(self, app, options):
         self._services = [
